069fb92096341d588c9c285f70b920139363099b
Chetan Sharma <Chetan.Sharma@cerner.com>
2023-09-03 20:59:09 +0530

    Home page

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -26,3 +26,38 @@ export const createThread = async ({text, author, communityId, path }: Params): 
 
     revalidatePath(path);
 };
+
+export const fetchThreads = async (pageNumber = 1, pageSize = 20) => {
+    connectToDB();
+    const threadSkipAmmount = (pageNumber - 1) * pageSize;
+
+    const threadsQuery = Thread.find({
+        parentId: { $in: [null, undefined] }
+    })
+    .sort({
+        createdAt: 'desc'
+    })
+    .skip(threadSkipAmmount)
+    .limit(pageSize)
+    .populate({ 
+        path: 'author', model: User
+    })
+    .populate({
+        path: 'children',
+        populate: {
+            path: 'author',
+            model: User,
+            select: "_id name parentId image"
+        }
+    });
+
+    const totalThreadsCount = await Thread.countDocuments({
+        parentId: { $in: [null, undefined] }
+    });
+
+    const threads = await threadsQuery.exec();
+
+    const isNext = totalThreadsCount > threadSkipAmmount | threads.length;
+
+    return { threads, isNext };
+}
